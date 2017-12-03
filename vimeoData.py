c2afee429021b4cae92d47f8136c8be42e1e9e69
loveyust <charles.yust@frogdesign.com>
2017-12-03 03:37:07 -0800

    Experiment with PJS Streamgraph, switch to D3 Streamgraph

--- vimeoData.py
+++ vimeoData.py
@@ -8,6 +8,7 @@ import re
 import operator
 from operator import attrgetter
 from operator import itemgetter
+import csv
 
 # Access the vimeo API
 # v = vimeo.VimeoClient(token="15788d8848c4bd4d5bc044ea110e7d52", key="a549d0fcfcf1ac4456c83f4dd5788364a00ddd3c", secret="37sdo9zfjEieq7UB0gKRR41xxfuz3nzW7GmIL770lTJ4L5gUcGpvQGGlYjIWfaqlZvEoEFZPs6KxnLqQgV7RGu2bAalm86iV5esLnz81fxXeJBx0Fxuwr04oNJfp2QaG")
@@ -56,7 +57,7 @@ for vid in data_in:
         if '2011' in name: # 2011 videos have the name at the front
             name = name.split(' - ',1)[0]
         elif ' - ' in name:
-            print name
+            # print name
             name = name.split(' - ',1)[1]
 
         # remove nums
@@ -77,7 +78,7 @@ for vid in data_in:
                 names = name.split(' and ',1)
             elif '&' in name:
                 names = name.split(' & ',1)
-                print names
+                # print names
             else:
                 names.append(name)
 
@@ -102,8 +103,11 @@ for vid in data_in:
             #     else:
             #         data_out['people'][n]['num'] += 1
 
-        first_name = name.split(' ',1)[0]
-        first_name = first_name.lower()
+        name_full = name.split(' ',1)
+        first_name = name_full[0].lower()
+        last_name = ''
+        if (len(name_full) > 1):
+            last_name = name_full[1].lower()
 
         talk = {}
         talk['description'] = vid[u'description']
@@ -113,8 +117,8 @@ for vid in data_in:
         dt = datetime.strptime(vid[u'release_time'], '%Y-%m-%dT%H:%M:%S+00:00')
         y = dt.strftime('%Y')
 
-        if y not in data_out['tags']:
-            data_out['tags'][y] = {}
+        #if y not in data_out['tags']:
+        #    data_out['tags'][y] = {}
 
         # if (vid[u'tags'][0][u'name'] == u'eyeo2017'):
             # print vid[u'tags'][0][u'name']
@@ -122,29 +126,116 @@ for vid in data_in:
         for tag in vid[u'tags']:
 
             # lowercase
-            tag[u'name'] = tag[u'name'].lower();
+            current_tag = tag[u'name'].lower();
 
             # remove unwanted characters
             for char in ' -_#@':
-                tag[u'name'] = tag[u'name'].replace(char,'')
+                current_tag = current_tag.replace(char,'')
+
+            # coorect some misspellings
+            if (current_tag == 'datavisaulization'):
+               current_tag = 'datavisualization'
+            elif (current_tag == 'datviz'):
+               current_tag = 'dataviz'
+            elif (current_tag == 'processing2.0'):
+               current_tag = 'processing'
+            elif (current_tag == 'datastorie'):
+               current_tag = 'datastories'
+            elif (current_tag == 'fatlabs'):
+               current_tag = 'fatlab'
+            elif (current_tag == 'interactiveenvironmnet'):
+               current_tag = 'interactiveenvironment'
+            elif (current_tag == 'interactiveinstallations'):
+               current_tag = 'interactiveinstallation'
+            elif (current_tag == 'of'):
+               current_tag = 'openframeworks'
+            elif (current_tag == '9/11memorial'):
+               current_tag = '911memorial'
+
+            # datavisualization, datavisaulization, dataviz, datviz
+            # fatlab, fatlabs
+            # interactiveenvironmnet
+            # infographics, infoviz
 
             # Remove unwanted tags - interactiveInstallation
             match = False
-            ignore_strings = {'Ignite', 'ignite', 'eyeo', 'inst-int', 'INST_INT', 'Eyeo', 'instint', first_name}
+            ignore_strings = {'Ignite', 'ignite', 'eyeo', 'yeo', 'inst-int', 'INST_INT', 'Eyeo', 'instint', first_name, last_name}
             for istr in ignore_strings:
-                if istr in tag[u'name']:
+                if istr in current_tag:
                     match = True
 
             if match is False:
-                if tag[u'name'] not in data_out['tags'][y]:
-                    data_out['tags'][y][tag[u'name']] = {}
-                    data_out['tags'][y][tag[u'name']]['num'] = 1
+                # if current_tag not in data_out['tags'][y]:
+                #     data_out['tags'][y][current_tag] = {}
+                #     data_out['tags'][y][current_tag]['num'] = 1
+                # else:
+                #     data_out['tags'][y][current_tag]['num'] += 1
+                if current_tag not in data_out['tags']:
+                    data_out['tags'][current_tag] = {}
+                    data_out['tags'][current_tag][y] = {}
+                    data_out['tags'][current_tag][y]['num'] = 1
                 else:
-                    data_out['tags'][y][tag[u'name']]['num'] += 1
+                    if y not in data_out['tags'][current_tag]:
+                        data_out['tags'][current_tag][y] = {}
+                        data_out['tags'][current_tag][y]['num'] = 1
+                    else:
+                        data_out['tags'][current_tag][y]['num'] += 1
 
 data_out['people'].sort(key=itemgetter("num"), reverse=True)
-print data_out['people']
 
 with open('data.json', 'w') as outfile:
     json.dump(data_out, outfile)
+
+# Write to a D3 ready csv
+data_csv = [['key','value','date']];
+
+for t in data_out['tags']:
+    
+    b_ignore = False
+    if (len(data_out['tags'][t]) == 1):
+        for y in data_out['tags'][t]:
+            # print t + ' ' + str(data_out['tags'][t][y]['num'])
+            if (data_out['tags'][t][y]['num'] == 1):
+                b_ignore = True
+    if b_ignore == False:
+        new_data_csv_wrapper = []
+        for x in range(0, 8):
+            new_data_csv = []
+            new_data_csv.append(t)
+            new_data_csv.append(0)
+            s = str(2011 + x) + '0101'
+            date = datetime(year=int(s[0:4]), month=int(s[4:6]), day=int(s[6:8]))
+            s = date.strftime("%m/%d/%y")
+            new_data_csv.append(s)
+            new_data_csv_wrapper.append(new_data_csv)
+            #print "We're on time %d" % (x)
+
+        for y in data_out['tags'][t]:
+
+            s = str(y) + '0101'
+            date = datetime(year=int(s[0:4]), month=int(s[4:6]), day=int(s[6:8]))
+            s = date.strftime("%m/%d/%y")
+
+            for d in range (0, 8):
+                if (s == new_data_csv_wrapper[d][2]):
+                    # found matching date, change the score
+                    new_data_csv_wrapper[d][1] = data_out['tags'][t][y]['num'] # / 100.0
+
+            # new_data_csv.append(data_out['tags'][t][y]['num'] / 100.0)
+            # s = str(y) + '0101'
+            # date = datetime(year=int(s[0:4]), month=int(s[4:6]), day=int(s[6:8]))
+            # s = date.strftime("%m/%d/%y")
+            # new_data_csv.append(s)
+
+        for i in range(len(new_data_csv_wrapper)):
+            data_csv.append(new_data_csv_wrapper[i])
+
+csv.register_dialect('myDialect', delimiter=',', quoting=csv.QUOTE_NONE)
+myFile = open('data_tags.csv', 'w')
+with myFile:
+   writer = csv.writer(myFile, dialect='myDialect')
+   writer.writerows(data_csv)
+
+
+
 
